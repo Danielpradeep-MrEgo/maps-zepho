80d122ccf81b2983e9db66375e7042bf864f3288
Danny <Danielpradeep0@gmail.com>
2022-02-14 06:03:33 +0530

    maps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
+import { Geolocation } from '@capacitor/geolocation';
+
+interface MapData {
+  latitude: number;
+  longitude: number;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  @ViewChild('map') mapView: ElementRef;
+
+  async ionViewDidEnter() {
+    const boundingRect =
+      this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
+
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    const data: MapData = {
+      latitude: coordinates.coords.latitude,
+      longitude: coordinates.coords.longitude,
+    };
+
+    CapacitorGoogleMaps.create({
+      width: Math.round(boundingRect.width),
+      height: Math.round(boundingRect.height),
+      x: Math.round(boundingRect.x),
+      y: Math.round(boundingRect.y),
+
+      latitude: data.latitude,
+      longitude: data.longitude,
+      zoom: 12,
+    });
+
+    CapacitorGoogleMaps.addListener('onMapReady', async function () {
+      CapacitorGoogleMaps.addMarker({
+        latitude: data.latitude,
+        longitude: data.longitude,
+        title: 'Emergency',
+        snippet: 'severe trouble',
+      });
+
+      CapacitorGoogleMaps.setMapType({
+        type: 'normal',
+      });
+    });
+
+    console.log(data.latitude, '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+  }
+
+  ionViewDidLeave() {
+    CapacitorGoogleMaps.close();
+  }
 }
